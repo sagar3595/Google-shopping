8bba0d227d77861adf7aca645a35ff5906b2cd96
sagar3595 <sagar47kumar13@gmail.com>
2023-08-17 18:12:14 +0530

    first commit

---
+++ app/search/[term]/page.tsx
@@ -0,0 +1,40 @@
+import ResultsList from "@/components/ResultsList";
+import { getFetchUrl } from "@/lib/getFetchUrl";
+import { PageResult, SearchParams } from "@/typings";
+import { redirect } from "next/navigation";
+
+type Props = {
+    searchParams: SearchParams;
+    params: {
+        term: string;
+    };
+};
+
+
+ async function SearchPage({ searchParams, params: {term} }: Props) {
+    if(!term){
+        redirect('/');
+    }
+
+    //fetch from API...
+
+    const response = await fetch(getFetchUrl('api/search'), {
+        method: 'POST',
+        body: JSON.stringify({ searchTerm: term, ...searchParams }),
+    });
+
+    const results = (await response.json()) as PageResult[];
+
+    console.log(results);
+
+
+
+
+  return (
+    <div>
+      <ResultsList results = {results} term = {term} />
+    </div>
+  )
+}
+
+export default SearchPage
